a3b4a27de3ad52b5ae431558e163a256ac5a6931
Nilu1002 <niladrimondal1002@gmail.com>
2025-11-21 15:00:19 +0530

    feat: add order queue

---
+++ src/queue/orderQueue.ts
@@ -0,0 +1,20 @@
+import { Queue } from 'bullmq';
+import IORedis from 'ioredis';
+import { config } from '../config/env';
+import { OrderJobData } from '../types/order';
+
+const connection = new IORedis(config.redis.url);
+
+export const orderQueue = new Queue<OrderJobData>(config.queue.name, { connection });
+
+export const enqueueOrder = (job: OrderJobData) =>
+  orderQueue.add('execute-order', job, {
+    attempts: config.queue.maxAttempts,
+    backoff: {
+      type: 'exponential',
+      delay: config.queue.backoffMs,
+    },
+    removeOnComplete: true,
+    removeOnFail: false,
+  });
+
