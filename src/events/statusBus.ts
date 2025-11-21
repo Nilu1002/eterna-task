da5d83f31d5bb82ed429335cd4617a711722f019
Nilu1002 <niladrimondal1002@gmail.com>
2025-11-21 14:59:41 +0530

    feat: add event bus for status updates

---
+++ src/events/statusBus.ts
@@ -0,0 +1,27 @@
+import { EventEmitter } from 'node:events';
+import { OrderStatus, StatusUpdate } from '../types/order';
+import { orderStore } from '../store/orderStore';
+
+type StatusListener = (update: StatusUpdate) => void;
+
+class StatusBus extends EventEmitter {
+  constructor() {
+    super();
+    this.setMaxListeners(0);
+  }
+
+  emitStatus(orderId: string, status: OrderStatus, detail?: unknown) {
+    const update = orderStore.appendStatus(orderId, status, detail);
+    if (update) {
+      super.emit(orderId, update);
+    }
+  }
+
+  subscribe(orderId: string, listener: StatusListener) {
+    super.on(orderId, listener);
+    return () => super.off(orderId, listener);
+  }
+}
+
+export const statusBus = new StatusBus();
+
